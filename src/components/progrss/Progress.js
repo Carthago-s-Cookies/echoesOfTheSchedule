edb7eb5fc335c93a5dba1d5a9fac9e07b1f3025a
IgolJack <iegolepic@gmail.com>
2020-11-08 17:53:46 +0300

    add table

--- src/components/progrss/Progress.js
+++ src/components/progrss/Progress.js
@@ -8,39 +8,62 @@ class Progress extends React.Component {
     this.state = {
       data: [
         {
-          key: "1",
+          key: "mark081120",
           name: "Лина",
-          1: 3,
-          2: 4,
-          3: 5,
-          4: 1,
+          mark081120: 3,
+          mark091120: 4,
+          mark101120: 5,
+          mark111120: 1,
         },
         {
-          key: "2",
+          key: "mark091120",
           name: "Женя",
-          1: 2,
-          2: 5,
-          3: 1,
-          4: 1,
+          mark081120: 2,
+          mark091120: 5,
+          mark101120: 1,
+          mark111120: 1,
         },
         {
-          key: "3",
+          key: "mark101120",
           name: "Малина",
-          1: 1,
-          2: 1,
-          3: 1,
-          4: 1,
+          mark081120: 1,
+          mark091120: 1,
+          mark101120: 1,
+          mark111120: 1,
         },
         {
-          key: "4",
+          key: "mark111120",
           name: "Вишня",
-          1: 0,
-          2: 0,
-          3: 0,
-          4: 1,
+          mark081120: 0,
+          mark091120: 0,
+          mark101120: 0,
+          mark111120: 1,
         },
       ],
     };
+  }
+
+  changeValue = (record, dat, event) => {
+    let newData = this.state.data
+    let serachI = 0
+
+    let i = 0;
+    while (i < this.state.data.length) {
+      if (record.key == this.state.data[i].key){
+        serachI = i
+      }
+      i++;
+    }
+    console.log("Номер элемента в массиве > ", serachI)
+    console.log("Искомое значение > ", dat )
+    console.log("Объект в котором ищем > ", this.state.data[serachI])
+    console.log("Нужное значение > ", this.state.data[serachI][dat])
+    console.log(event.target.value)
+    
+    newData[serachI][dat] = Number(event.target.value)
+    console.log("Нужное значение после обновления > ", newData[serachI][dat])
+    this.setState({data: newData}, () => console.log(this.state.data))
+
   }
 
   render() {
@@ -50,13 +73,22 @@ class Progress extends React.Component {
           title={"Столибик " + dat.key}
           dataIndex={dat.key}
           name={dat.key}
+          render={(text, record) => (
+            <>
+              <Input bordered={false} defaultValue={text}  onChange={ (event) =>  this.changeValue(record, dat.key, event)}></Input>
+            </>
+          )}
         />
       );
     });
 
     return (
       <>
-        <Table dataSource={this.state.data} size="small" pagination={false}  rowKey={record => console.log(record)}>
+        <Table
+          dataSource={this.state.data}
+          size="small"
+          pagination={false}
+        >
           <Column title="ФИО" dataIndex="name" name={this.state.data.name} />
           {door}
         </Table>
